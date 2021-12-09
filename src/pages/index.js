35971fcce80da86fc2529d4c828ab7d749e02f88
DESKTOP-P19E6MM\G. Scott Cutrer <gcutrer@gmail.com>
2021-12-09 09:02:01 -0600

    Part 3 change.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,14 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
 
       <StaticImage
-        alt="The family at Thanksgiving 2020"
-        src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/200481752_10109981146140217_622478557431802745_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=5RrCSMVcXnMAX9YwoUA&_nc_ht=scontent-dfw5-1.xx&oh=f24e2bea27efafce69e35141a6b52d65&oe=61D65220"
-      />
-
-      <StaticImage
         alt="Juliana and Peaches and Gauge"
         src="..\images\IMG_20211015_091032_3.jpg"
+      />
+
+
+    <StaticImage
+        alt="The family at Thanksgiving 2020"
+        src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/200481752_10109981146140217_622478557431802745_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=5RrCSMVcXnMAX9YwoUA&_nc_ht=scontent-dfw5-1.xx&oh=f24e2bea27efafce69e35141a6b52d65&oe=61D65220"
       />
 
     </Layout>
